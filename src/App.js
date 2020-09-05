6c0bff3f63bf1bfbf4ab6d1e6365c96423e1dde9
denskarlet <den.skarlet@gmail.com>
2020-09-05 14:56:17 -0400

    very basic classs reducer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { act } from 'react-dom/test-utils';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  count: 0,
+};
+
+let currentState = initialState;
+
+// basic reducer
+function countReducer(state = initialState, { type, payload }) {
+  if (type === 'increment')
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  if (type === 'decrement')
+    return {
+      ...state,
+      count: state.count - 1,
+    };
+  if (type === 'reset')
+    return {
+      ...state,
+      count: 0,
+    };
+  return state;
 }
-var s = 4;
+// very basic dispatch that returns updated state.. because it's really basic
+function dispatch(action) {
+  currentState = countReducer(currentState, action);
+  return currentState;
+}
+function incrementCount() {
+  const action = { type: 'increment' };
+  return dispatch(action);
+}
+function decrementCount() {
+  const action = { type: 'decrement' };
+  return dispatch(action);
+}
+function resetCount() {
+  const action = { type: 'reset' };
+  return dispatch(action);
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = currentState;
+  }
+  increment = () => {
+    this.setState(incrementCount());
+  };
+  decrement = () => {
+    this.setState(decrementCount());
+  };
+  reset = () => {
+    this.setState(resetCount());
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>{' '}
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    );
+  }
+}
 
 export default App;
