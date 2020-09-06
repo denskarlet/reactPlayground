26dcc808a0644d72ee9a4201c590ee27054671c7
denskarlet <den.skarlet@gmail.com>
2020-09-06 18:46:47 -0400

    hooks with useCallback

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const increment = useCallback(() => setCount((count) => count + 1), [setCount]);
+  const decrement = useCallback(() => setCount((count) => count - 1), [setCount]);
+  const reset = useCallback(() => setCount(0), [setCount]);
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
