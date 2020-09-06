f11a2b75bd355a4acd1e73335795a1e735c4a99c
denskarlet <den.skarlet@gmail.com>
2020-09-06 18:38:03 -0400

    hooks

---
+++ src/Counter.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
